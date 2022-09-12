87a9c90204f5ba5de0e4de239a63ddaf5b05fe87
Sally <jw4074@columbia.edu>
2022-09-12 12:32:27 -0400

    first contract

---
+++ smartcontact/contracts/contract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract Contract is Ownable {
+    uint256 private _value;
+
+    event ValueChanged(uint256 value);
+
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 value) public onlyOwner {
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
